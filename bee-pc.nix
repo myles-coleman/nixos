2f0ac5d5351581c7ba477555b0be30ddce94cfcd
Myles Coleman <mylescoleman05@gmail.com>
2025-06-06 23:17:58 -0700

    35 current  2025-06-06 23:17:55  25.05.802746.7282cb574e06  6.12.30                          *

--- bee-pc.nix
+++ bee-pc.nix
@@ -124,7 +124,14 @@ in {
     mesa-demos
   ];
 
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia.open = true;
+
   environment.variables = {
+    # LD_LIBRARY_PATH = "/run/opengl-driver/lib:/run/opengl-driver-32/lib:${x11LibPath}:${xextLibPath}:${xrandrLibPath}";
+    # VK_LAYER_PATH = "${pkgs.mangohud}/etc/vulkan/implicit_layer.d";
+
+    # GLOBAL MANGOHUD ENABLE
     MANGOHUD = "1";
     VK_INSTANCE_LAYERS = "VK_LAYER_MANGOHUD_overlay";
   };
