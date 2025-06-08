2b9978ab7899b49bbe8d2fe4c3c1844008315991
Myles Coleman <mylescoleman05@gmail.com>
2025-06-07 18:59:00 -0700

    75 current  2025-06-07 18:58:55  25.05.802746.7282cb574e06  6.12.30                          *

--- bee-test.nix
+++ bee-test.nix
@@ -43,7 +43,7 @@
     graphics.enable = true;
     nvidia.modesetting.enable = true;
     nvidia.open = false;
-    nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+    nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
     nvidia.powerManagement.enable = false;
     nvidia.powerManagement.finegrained = false;
     nvidia.nvidiaSettings = true;
