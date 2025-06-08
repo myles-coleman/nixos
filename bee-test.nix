041a609e265d57efb7ec4651faa5f97494c277d4
Myles Coleman <mylescoleman05@gmail.com>
2025-06-08 16:10:08 -0700

    96 current  2025-06-08 16:10:02  25.05.802746.7282cb574e06  6.14.6-xanmod1                          *

--- bee-test.nix
+++ bee-test.nix
@@ -4,8 +4,9 @@
   lib,
   ...
 }: {
-  # Using a specific kernel version that's compatible with NVIDIA drivers
-  boot.kernelPackages = pkgs.linuxPackages_6_1;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_6_1;
+
   boot.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
   boot.kernelParams = ["nvidia-drm.modeset=1"];
 
