77d2498b342163fceea5328579c2f9569fa7316f
Myles Coleman <mylescoleman05@gmail.com>
2025-06-08 16:49:04 -0700

    98 current  2025-06-08 16:48:59  25.05.802746.7282cb574e06  6.14.6-xanmod1                          *

--- bee-test.nix
+++ bee-test.nix
@@ -4,8 +4,8 @@
   lib,
   ...
 }: {
-  # boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
-  boot.kernelPackages = pkgs.linuxPackages_6_1;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_6_1;
 
   boot.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm"];
   boot.kernelParams = ["nvidia-drm.modeset=1"];
