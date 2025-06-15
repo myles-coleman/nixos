eb331a500dbfb84c4504469667dbac06cd5d9e96
Myles Coleman <mylescoleman05@gmail.com>
2025-06-15 13:09:06 -0700

    109 current  2025-06-15 13:09:01  25.05.802746.7282cb574e06  6.12.30                                 *

--- bee-pc.nix
+++ bee-pc.nix
@@ -5,7 +5,29 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  krisp-patcher =
+    pkgs.writers.writePython3Bin "krisp-patcher"
+    {
+      libraries = with pkgs.python3Packages; [
+        capstone
+        pyelftools
+      ];
+      flakeIgnore = [
+        "E501" # line too long (82 > 79 characters)
+        "F403" # 'from module import *' used; unable to detect undefined names
+        "F405" # name may be undefined, or defined from star imports: module
+      ];
+    }
+    (
+      builtins.readFile (
+        pkgs.fetchurl {
+          url = "https://pastebin.com/raw/8tQDsMVd";
+          sha256 = "sha256-IdXv0MfRG1/1pAAwHLS2+1NESFEz2uXrbSdvU9OvdJ8=";
+        }
+      )
+    );
+in {
   services.xserver = {
     enable = true;
     displayManager.gdm = {
@@ -86,6 +108,7 @@
     xorg.libX11
     mesa-demos
     mangohud
+    krisp-patcher
   ];
 
   environment.variables = {
