71bc92ff9d5ca85b56c3c7c41c80e062af38b183
myles-coleman <mylescoleman05@gmail.com>
2025-08-11 19:34:28 -0700

    add rpi5 build

---
+++ rpi5/flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "NixOS configuration for Raspberry Pi 5";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
+  };
+
+  outputs = { self, nixpkgs, nixos-raspberrypi }@inputs:
+    let
+      system = "x86_64-linux";  # build system architecture
+      targetSystem = "aarch64-linux";  # target architecture
+    in {
+      
+      packages.${system} = {
+        sdImage = nixos-raspberrypi.packages.${system}.sdImages.rpi5.override {
+          config = self.nixosConfigurations.rpi5;
+        };
+      };
+
+      nixosConfigurations.rpi5 = nixos-raspberrypi.lib.nixosSystem {
+        inherit system;
+        specialArgs = inputs;
+        modules = [
+          ({...}: {
+            imports = with nixos-raspberrypi.nixosModules; [
+              raspberry-pi-5.base
+              raspberry-pi-5.bluetooth
+            ];
+          })
+          
+          {
+            networking = {
+              hostName = "node1";
+            };
+            
+            users.users.pi = {
+              initialPassword = "raspberry";
+              isNormalUser = true;
+              extraGroups = [ "wheel" ];
+            };
+            
+            services.openssh = {
+              enable = true;
+              settings.PasswordAuthentication = true;
+              settings.PermitRootLogin = "no";
+            };
+            
+            time.timeZone = "America/Los_Angeles";
+            i18n.defaultLocale = "en_US.UTF-8";
+            
+            environment.systemPackages = with nixpkgs.legacyPackages.${targetSystem}; [
+              vim
+              git
+              htop
+            ];
+            
+            hardware.enableRedistributableFirmware = true; # Hardware-specific settings
+            system.stateVersion = "25.05"; # This is required for the SD image
+          }
+        ];
+      };
+    };
+}
