1ae2d9858f32420bf1986cc2f009f095ca3b1835
Myles Coleman <mylescoleman05@gmail.com>
2025-10-04 19:18:33 -0700

    52 current  2025-10-04 19:18:30  25.05.807819.e728d7ae4bb6  6.12.41                          *

--- bee-test.nix
+++ bee-test.nix
@@ -22,7 +22,7 @@ in {
     syntaxHighlighting.enable = true;
     enableCompletion = true;
     shellAliases = {
-      rebuild = "./${config.users.users.${mainUser}.home}/nixos/rebuild.sh";
+      rebuild = ".${config.users.users.${mainUser}.home}/nixos/rebuild.sh";
     };
     ohMyZsh = {
       enable = true;
