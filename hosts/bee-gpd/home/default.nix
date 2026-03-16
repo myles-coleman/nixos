c88e7807b499331379763bcd4f03693c6cd880a1
Myles Coleman <mylescoleman05@gmail.com>
2026-03-15 20:58:40 -0700

    34 current  2026-03-15 20:58:38  25.05.20260102.ac62194     6.12.63                          *

--- hosts/bee-gpd/home/default.nix
+++ hosts/bee-gpd/home/default.nix
@@ -7,6 +7,22 @@
   mainUser = "bee";
 in {
   home-manager.users.${mainUser} = {
+    services.redshift = {
+      enable = true;
+      latitude = 37.7749;
+      longitude = -122.4194;
+      temperature = {
+        day = 5500;
+        night = 3500;
+      };
+      settings = {
+        redshift = {
+          brightness-day = "1.0";
+          brightness-night = "0.8";
+        };
+      };
+    };
+
     xdg.configFile = {
       "hypr/hyprland.conf".source = ./hyprland.conf;
       "hypr/start.sh" = {
