0835d4bf58c4935461297ff5c1f51ed68f64d05c
Myles Coleman <mylescoleman05@gmail.com>
2026-03-16 19:52:01 -0700

    40 current  2026-03-16 19:51:59  25.05.20260102.ac62194     6.12.63                          *

--- modules/home.nix
+++ modules/home.nix
@@ -18,7 +18,7 @@ in {
     home.pointerCursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Classic";
-      size = 16;
+      size = 20;
       gtk.enable = true;
       x11.enable = true;
     };
